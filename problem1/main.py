77bc8124397e4f1e84df929629d0010bba434b1d
masgal <dimasrumekso28@gmail.com>
2024-05-13 19:42:10 +0700

    solution1

--- problem1/main.py
+++ problem1/main.py
@@ -5,13 +5,12 @@ def prime_number(number):
         return True
     elif number % 2 == 0 or number % 3 == 0 :
         return False
-    elif number <= 5:
-        return True
-    else :
-        for i in range(5,9):
-            if number % i == 0:
-                return False
-            return True
+    i = 5
+    while i * i <= number :
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
 if __name__ == '__main__':
